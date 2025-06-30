12af181291fc4ffa1009d5422e5b3786e06f92c6
Aayush0966 <aayushx699@gmail.com>
2025-07-01 00:46:11 +0545

    feat: integrate socket.io for real-time messaging and search functionality for messages

--- server/src/routes/message.routes.ts
+++ server/src/routes/message.routes.ts
@@ -5,6 +5,7 @@ import { messageController } from "../controllers/message.controller";
 export const messageRouter = express.Router();
 
 messageRouter.post("/", validateUser, messageController.sendMessage)
+messageRouter.get("/search", validateUser, messageController.searchMessage)
 messageRouter.get("/:chatId", validateUser, messageController.getMessageByChat)
 messageRouter.delete("/user/:messageId", validateUser, messageController.removeMessageForYourself)
 messageRouter.delete("/both/:messageId", validateUser, messageController.removeMessageForBoth)
